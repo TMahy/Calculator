d5c983e21a0a2fdc60b79a19c8c7b61a12be5193
Tom Mahy <tommahy11@gmail.com>
2023-05-30 17:59:15 +0100

    Updates

--- index.js
+++ index.js
@@ -1,3 +1,85 @@
+let displayVal = '0';
+let num1 = null; 
+let num2 = null; 
+let operator1 = null;
+let operator2 = null;
+let result = null;
+
+const buttons = document.querySelectorAll('button');
+const display = document.getElementById('display');
+
+function updateDisplay(){
+    display.innerText = displayVal;
+}
+
+updateDisplay();
+
+buttons.forEach(button =>{
+    button.addEventListener('click', function(){
+        if(button.classList.contains('allClear')){
+            clearDisplay(true);
+        }
+        if(button.classList.contains('clear')){
+            clearDisplay(false);
+        }
+        if(button.classList.contains('digit')){
+            inputDigit(button.value);
+        }
+        if(button.classList.contains('operator')){
+           inputOperator(button.value);
+        }
+        if(button.classList.contains('equals')){
+            inputEquals();
+        }
+        if(button.classList.contains('decimal')){
+            inputDecimal();
+        }
+        if(button.classList.contains('sign')){
+            inputSign(displayVal);   
+        }
+
+        updateDisplay()
+    })
+})
+
+function clearDisplay(allClear){
+    if(allClear){
+        displayVal = '0';
+        num1 = null; 
+        num2 = null; 
+        operator1 = null;
+        operator2 = null;
+        result = null;
+        return; 
+    } else{
+        return;
+    }
+}
+
+function inputDigit(digit){
+    if(displayVal == 0){
+        displayVal = digit;
+    }else{
+        displayVal += digit;
+    }
+}
+
+function inputOperator(operator){
+    console.log(displayVal, operator)
+}
+
+function inputEquals(){
+    console.log(displayVal, 'equals')
+}
+
+function inputDecimal(){
+    console.log('Not implemented yet')
+}
+
+function inputSign(value){
+    console.log('Not implemented yet')
+}
+
 function add(a, b){
     return a + b;
 }
@@ -29,7 +111,3 @@ function operate(a, b, operator){
             return divide(a,b);
     }
 }
-
-let var1 = 0; 
-let var2 = 0; 
-let operator = '';

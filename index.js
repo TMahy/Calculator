c861a75db48c8b7697f6e30fdb583188d0f6d8e7
Tom Mahy <tommahy11@gmail.com>
2023-05-30 12:04:54 +0100

    Added basic arithmetic function to JS

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    if(b === 0){
+        return 'error'
+    }
+    return a/b;
+}
+
+function operate(a, b, operator){
+    switch(operator){
+        case '+':
+            return add(a,b);
+        case '-':
+            return subtract(a,b);
+        case '*':
+            return multiply(a,b);
+        case '/':
+            return divide(a,b);
+    }
+}
+
+let var1 = 0; 
+let var2 = 0; 
+let operator = '';
